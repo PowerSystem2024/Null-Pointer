59eb91a839f23b65033e45469f1bf68ddb399ef4
Cristian Gimenez <gimenezc779@gmail.com>
2024-11-09 16:15:28 -0300

    Agrego la seccion experiencia y algunas modificaciones en la linea de tiempo

--- Tecnicatura/PortafolioWeb/src/components/ProyectosComponente.vue
+++ Tecnicatura/PortafolioWeb/src/components/ProyectosComponente.vue
@@ -20,9 +20,23 @@ misProyectos.value = ([
         projectoLink: "https://www.google.com.ar",
         githubLink: "https://www.github.com"
     },
-
+    {
+        id: 1,
+        src: proyecto1,
+        titulo: "Ahorcado en pseint",
+        description: "Con el grupo de la tecnicatura hicimos un juego llamado ahorcado en Pseint",
+        projectoLink: "https://www.google.com.ar",
+        githubLink: "https://www.github.com"
+    },
+    {
+        id: 2,
+        src: proyecto2,
+        titulo: "Ahorcado en Java",
+        description: "Con el grupo de la tecnicatura hicimos un juego llamado ahorcado en lenguaje de Java",
+        projectoLink: "https://www.google.com.ar",
+        githubLink: "https://www.github.com"
+    },
 ])
-
 </script>
 
 <template>
@@ -33,9 +47,9 @@ misProyectos.value = ([
                 <h3>{{ proyecto.titulo }}</h3>
                 <p>{{ proyecto.description }}</p>
                 <div class="proyecto-links">
-                    <a href="proyecto.proyectoLink" class="btn-ver-mas" target="_blank" rel="noopener noreferrer">Ver
+                    <a :href="proyecto.proyectoLink" class="btn-ver-mas" target="_blank" rel="noopener noreferrer">Ver
                         Proyecto </a>
-                    <a href="proyecto.githubLink" class="github-link" target="_blank" rel="noopener noreferrer"> Ver
+                    <a :href="proyecto.githubLink" class="github-link" target="_blank" rel="noopener noreferrer"> Ver
                         Codigo en GitHub</a>
                 </div>
             </div>
@@ -44,19 +58,6 @@ misProyectos.value = ([
 </template>
 
 <style scoped>
-/* .galeria{
-    width: 100%;
-    height: 100vh;
-    display: flex;
-    flex-wrap: wrap;
-    gap: 20px;
-    justify-content: center;
-    background-image: url('/src/assets/foto-proyecto.jpg');
-    background-size: cover;
-    background-position: center;
-    background-repeat: no-repeat;
-} */
-
 .galeria {
     width: 100%;
     height: 100%;
@@ -65,9 +66,9 @@ misProyectos.value = ([
     gap: 20px;
     padding: 20px;
     justify-content: center;
-    background: linear-gradient(-70deg, #161616, #13571f, #089754,#00ff3798);
+    background: linear-gradient(-70deg, #161616, #13571f, #089754, #00ff3798);
     background-size: 400% 400%;
-    animation: gradient 10s ease infinite
+    animation: gradient 10s ease infinite;
 }
 
 @keyframes gradient {
@@ -80,8 +81,8 @@ misProyectos.value = ([
     100% {
         background-position: 0% 50%;
     }
-
 }
+
 .proyecto {
     display: flex;
     flex-direction: column;
@@ -92,31 +93,38 @@ misProyectos.value = ([
     max-width: 222px;
     flex: 1 1 300px;
 }
-.poyecto img {
+
+.proyecto img {
     width: 100%;
-    height: auto;
+    height: 200px;  /* Modificado para establecer un tamaño fijo */
+    object-fit: cover;  /* Ajusta la imagen para cubrir el área sin distorsionar */
     display: block;
 }
-.proyecto-info{
+
+.proyecto-info {
     padding: 15px;
     text-align: center;
 }
+
 .proyecto-info h3 {
     margin: 10px 0;
     font-size: 1.3rem;
     color: #333;
 }
+
 .proyecto-info p {
     margin: 10px 0;
     font-size: 1em;
     color: #666;
 }
+
 .proyecto-links {
     display: flex;
     flex-direction: column;
     gap: 10px;
     margin-top: 10px;
 }
+
 .proyecto-links .btn-ver-mas {
     background-color: #064700;
     color: #fff;
@@ -125,14 +133,17 @@ misProyectos.value = ([
     text-decoration: none;
     transition: background-color 0.3s;
 }
+
 .proyecto-links .btn-ver-mas:hover {
     background-color: #02b311;
 }
+
 .proyecto-links .github-link {
     color: #333;
     text-decoration: none;
     font-size: 0.9em;
 }
+
 .proyecto-links .github-link:hover {
     text-decoration: underline;
 }
